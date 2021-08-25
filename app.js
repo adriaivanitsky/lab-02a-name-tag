d962081d8ac6650d55ccd1b1be9f4abca8a21cc2
Adria Ivanitsky <littleivanitsky@gmail.com>
2021-08-24 19:00:55 -0700

    added eslint to vscode

---
+++ app.js
@@ -0,0 +1,6 @@
+bad.code
+
+/*simply a test to see if eslint is working correctly.
+if i hover my cursor over "code" it tells me i am missing
+a semi colon. helpful for learning wtf to code when u don't
+know shit!*/
